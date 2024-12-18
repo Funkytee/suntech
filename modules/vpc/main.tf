a5ab395241ec70f292a1bfa5d785165d66f0afd1
Anthony Ogumeh <tonyakinsy2k@yahoo.com>
2024-12-17 20:41:21 -0500

    added modifie code  for aws resources

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -44,3 +44,12 @@ resource "aws_route_table" "public_route_table" {
   Name = "Public-Route-Table"
  }
 }
+
+# Create S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-s3-bucket-name" # Replace with a globally unique name
+  # Enable versioning
+  versioning {
+    enabled = true
+  }
+}
